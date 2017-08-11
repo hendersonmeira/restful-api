69253216e0ebba3bcd1f7efc3f98704bb50c0725
Henderson Meira <dev.henderson.meira@gmail.com>
2017-08-11 20:52:42 -0300

    Implementando um servidorzinho na unha

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Desenvolvido com NodeJS</title>
+  </head>
+  <body>
+    <h1>Servidor na unha</h1>
+    <h2>Henderson Meira</h2>
+  </body>
+</html>`;
